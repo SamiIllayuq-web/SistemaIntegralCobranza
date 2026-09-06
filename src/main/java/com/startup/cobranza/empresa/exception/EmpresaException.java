ca28c90f78405240085c02367b6984758161bb31
SamiIllayuq <saminillayuq@gmail.com>
2026-09-06 01:23:58 -0500

    refactor: eliminar entidad Empresa y sus archivos (ya no existe en el modelo)

--- src/main/java/com/startup/cobranza/empresa/exception/EmpresaException.java
+++
@@ -1,7 +0,0 @@
-package com.startup.cobranza.empresa.exception;
-
-public class EmpresaException extends RuntimeException {
-    public EmpresaException(String message) {
-        super(message);
-    }
-}
